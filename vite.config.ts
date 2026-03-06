0f1e100303040ca3c2ec73a540ff0c37fc70f521
Keshan Chathuranga <keshanchathuranga777@gmail.com>
2026-03-06 15:41:54 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WRTS/',
+  base: '/wedding/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
